7b44393b34e22a5eb2ac04b4f65ddb6b4ac4231d
flyingray <flyingray@users.noreply.github.com>
2022-02-02 16:54:03 -0700

    motor control

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# using extension from url:
-# https://github.com/waveshare/JoyStick
-
-
 def process_send_queue():
     global snd_queue_value
     snd_queue_value = convert_to_text(send_queue.shift())
@@ -12,19 +8,15 @@ def draw_out_dot():
     led.plot_brightness(4, 2, 255)
     basic.pause(50)
     for an_element in [2, 1, 0]:
-        led.plot_brightness(4, 0, 255)
+        led.plot_brightness(4, an_element, 255)
         basic.pause(50)
     basic.pause(50)
     for an_element2 in [2, 1, 0]:
         led.plot_brightness(4, an_element2, 0)
         basic.pause(50)
-
-def send_message(message2: str):
-    send_queue.append(message2)
+def send_message(a_message: str):
+    send_queue.append(a_message)
     draw_out_dot()
-def send_move_direction(direction: str, xpos: number, ypos: number, image: Image):
-    send_message("move" + ":" + direction + "," + ("" + str(xpos)) + "," + ("" + str(ypos)))
-    image.show_image(0)
 
 def on_button_pressed_a():
     led.set_brightness(200)
@@ -36,36 +28,31 @@ def on_button_pressed_a():
                 . # . # .
     """).show_image(0)
     led.set_brightness(200)
-    send_message("action" + ":" + "a")
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def split_msg_name(message4: str):
-    return convert_to_text(message4.split(":")[0])
+
 def draw_in_dot():
     led.plot_brightness(4, 0, 255)
     basic.pause(50)
     for an_element3 in [0, 1, 2]:
-        value3 = 0
-        led.plot_brightness(4, value3, 255)
+        led.plot_brightness(4, an_element3, 255)
         basic.pause(50)
     basic.pause(50)
     for an_element4 in [0, 1, 2]:
         led.plot_brightness(4, an_element4, 0)
         basic.pause(50)
+
 def process_recv_queue():
     global recv_queue_value
     recv_queue_value = convert_to_text(recv_queue.shift())
     if recv_queue_value != "undefined":
         serial.write_line("recv:" + ("" + str(radio.received_packet(RadioPacketProperty.SERIAL_NUMBER))) + "," + ("" + str(radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH))) + "," + recv_queue_value)
-
-
 def show_bright_image(image2: Image):
     image2.show_image(0)
     led.set_brightness(200)
-def recv_message(message42: str):
-    recv_queue.append(message42)
+def recv_message(a_message2: str):
+    recv_queue.append(a_message2)
     draw_in_dot()
-
 
 def on_received_string(receivedString):
     recv_message(receivedString)
@@ -81,9 +68,7 @@ def on_button_pressed_b():
                 . # # . .
     """).show_image(0)
     led.set_brightness(200)
-    send_message("action" + ":" + "b")
 input.on_button_pressed(Button.B, on_button_pressed_b)
-
 
 def heartbeat():
     led.set_brightness(200)
@@ -100,7 +85,6 @@ def on_logo_pressed():
                 . . . . .
     """).show_image(0)
     led.set_brightness(200)
-    send_message("action" + ":" + "logo")
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
 recv_queue: List[str] = []
--- main.ts
+++ main.ts
@@ -13,8 +13,8 @@ function draw_out_dot () {
         basic.pause(50)
     }
     basic.pause(50)
-    for (let an_element of [2, 1, 0]) {
-        led.plotBrightness(4, an_element, 0)
+    for (let an_element2 of [2, 1, 0]) {
+        led.plotBrightness(4, an_element2, 0)
         basic.pause(50)
     }
 }
@@ -34,16 +34,15 @@ input.onButtonPressed(Button.A, function () {
     led.setBrightness(200)
 })
 function draw_in_dot () {
-    let value3: number;
-led.plotBrightness(4, 0, 255)
+    led.plotBrightness(4, 0, 255)
     basic.pause(50)
-    for (let an_element of [0, 1, 2]) {
-        led.plotBrightness(4, an_element, 255)
+    for (let an_element3 of [0, 1, 2]) {
+        led.plotBrightness(4, an_element3, 255)
         basic.pause(50)
     }
     basic.pause(50)
-    for (let an_element of [0, 1, 2]) {
-        led.plotBrightness(4, an_element, 0)
+    for (let an_element4 of [0, 1, 2]) {
+        led.plotBrightness(4, an_element4, 0)
         basic.pause(50)
     }
 }
@@ -51,14 +50,29 @@ function process_recv_queue () {
     recv_queue_value = convertToText(recv_queue.shift())
     if (recv_queue_value != "undefined") {
         serial.writeLine("recv:" + ("" + radio.receivedPacket(RadioPacketProperty.SerialNumber)) + "," + ("" + radio.receivedPacket(RadioPacketProperty.SignalStrength)) + "," + recv_queue_value)
+        if (recv_queue_value.split(":").shift() == "forward") {
+            Rover.MotorRunDual(50, 50)
+            images.arrowImage(ArrowNames.North).showImage(0)
+        } else if (recv_queue_value.split(":").shift() == "back") {
+            Rover.MotorRunDual(-50, -50)
+            images.arrowImage(ArrowNames.South).showImage(0)
+        } else if (recv_queue_value.split(":").shift() == "left") {
+            Rover.MotorRunDual(25, 75)
+            images.arrowImage(ArrowNames.West).showImage(0)
+        } else if (recv_queue_value.split(":").shift() == "right") {
+            Rover.MotorRunDual(75, 25)
+            images.arrowImage(ArrowNames.East).showImage(0)
+        } else {
+            Rover.MotorStopAll(MotorActions.Stop)
+        }
     }
 }
 function show_bright_image (image2: Image) {
     image2.showImage(0)
     led.setBrightness(200)
 }
-function recv_message (a_message: string) {
-    recv_queue.push(a_message)
+function recv_message (a_message2: string) {
+    recv_queue.push(a_message2)
     draw_in_dot()
 }
 radio.onReceivedString(function (receivedString) {
@@ -95,6 +109,7 @@ let recv_queue: string[] = []
 let recv_queue_value = ""
 let send_queue: string[] = []
 let snd_queue_value = ""
+Rover.MotorStopAll(MotorActions.Stop)
 radio.setTransmitSerialNumber(true)
 radio.setGroup(1)
 basic.showIcon(IconNames.SmallDiamond)
